5bdf0fd09c71361cc6d8dcc7ff7369af3bb9b526
Alexey Gruzdev <aleks-gruzdev92@yandex.ru>
2016-07-05 17:41:27 +0300

    Implemented gameplay

--- sample/source/TickTackToe/Main.cpp
+++ sample/source/TickTackToe/Main.cpp
@@ -24,7 +24,7 @@ using namespace multislider;
 #define VK_KEY_9 0x39
 
 class Controller
-    : public HostCallback, public ClientCallback
+    : public HostCallback, public ClientCallback, public SessionCallback
 {
     Host*      mHost    = nullptr;
     Client*    mClient  = nullptr;
@@ -33,6 +33,8 @@ class Controller
     bool mFinish = false;
 
     bool mHostPlaysCrosses = true;
+
+    uint32_t mTurn = 0;
 
     //-------------------------------------------------------
 
@@ -167,6 +169,11 @@ public:
         assert(mHost != nullptr || mClient != nullptr);
         setupScreen(roomname, mHost != nullptr);
 
+        //-------------------------------------------------------
+        // Game loop
+
+        assert(mSession != nullptr);
+        gameScreen(roomname, mHost != nullptr);
     }
 
     void setupScreen(const std::string & roomname, bool isHost)
@@ -176,10 +183,12 @@ public:
         // Setup the room
         //-------------------------------------------------------
 
+        bool finish = false;
+
         static const std::string CMD_PLAY_SYM = "play";
         static const std::string CMD_UPDATE = "update";
         static const std::string CMD_START = "start";
-        for (;;) {
+        while(!finish) {
             clear();
             std::cin.sync();
 
@@ -204,32 +213,133 @@ public:
             std::cout << std::endl;
 
             // Process command
+            bool wait = false;
             std::string cmdFull;
             std::getline(std::cin, cmdFull);
             auto cmd = splitCommand(cmdFull);
             if (cmd.size() == 2 && cmd[0] == CMD_PLAY_SYM) {
                 if (cmd[1] == "X" || cmd[1] == "O") {
                     mHost->broadcast(cmd[1], true);
+                    wait = true;
                 }
             }
-            else if (cmd.size() == 1 && cmd[0] == CMD_UPDATE) {
-                continue;
+            else if (cmdFull.empty() || (cmd.size() == 1 && cmd[0] == CMD_UPDATE)) {
+                /* pass */
+            }
+            else if (cmd.size() == 1 && cmd[0] == CMD_START) {
+                if (isHost) {
+                    mHost->startSession();
+                }
+                wait = true;
+                finish = true;
             }
             else {
                 std::cout << "Unknown command" << std::endl;
-                continue;
             }
-            
+
             uint32_t count = 0;
             do {
-                if (isHost) {
-                    count += mHost->receive();
-                }
-                else {
-                    count += mClient->receive();
-                }
-            } while (count == 0);
+                count += isHost 
+                    ? mHost->receive() 
+                    : mClient->receive();
+            } while ((0 == count) && wait);
         }
+    }
+
+
+    void gameScreen(const std::string & roomname, bool isHost)
+    {
+        while (!mFinish) {
+            mSession->receive();
+            pause(100);
+        }
+    }
+
+    void drawGameScreen(const std::string & roomname, const std::string & fieldData, bool myTurn, char mySymbol, int winCode)
+    {
+        clear();
+        std::cin.sync();
+
+        std::cout << "Room " << roomname << std::endl;
+        std::cout << "--------------------------------------------------" << std::endl;
+        std::cout << std::endl;
+        std::cout << "You play as " << mySymbol << std::endl;
+        std::cout << std::endl;
+
+        if (fieldData.size() == 9) {
+            std::cout << std::endl;
+            std::cout << "            " << " 1   2   3 " << std::endl;
+            std::cout << std::endl;
+            std::cout << "        a   " << " " << fieldData[0] << " | " << fieldData[1] << " | " << fieldData[2] << std::endl;
+            std::cout << "            " << "---|---|---" << std::endl;
+            std::cout << "        b   " << " " << fieldData[3] << " | " << fieldData[4] << " | " << fieldData[5] << std::endl;
+            std::cout << "            " << "---|---|---" << std::endl;
+            std::cout << "        c   " << " " << fieldData[6] << " | " << fieldData[7] << " | " << fieldData[8] << std::endl;
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
+
+        switch (winCode) {
+        default:
+        case 0:
+            if (myTurn) {
+                std::cout << "Your turn!" << std::endl;
+            }
+            else {
+                std::cout << std::endl;
+            }
+            break;
+        case 1:
+            std::cout << "You win!" << std::endl;
+            break;
+        case 2:
+            std::cout << "You lost!" << std::endl;
+            break;
+        case 3:
+            std::cout << "Draw!" << std::endl;
+            break;
+        }
+        std::cout << std::endl;
+    }
+
+    void unblockInput()
+    {
+        std::cin.putback('\n');
+    }
+
+    std::string makeMessage(const std::string & field, uint32_t turn)
+    {
+        return field + std::to_string(turn);
+    }
+
+    std::tuple<std::string, uint32_t> parseMessage(const std::string & msg)
+    {
+        return std::make_tuple(msg.substr(0, 9), static_cast<uint32_t>(std::stoul(msg.substr(9))));
+    }
+
+    char checkWin(const std::string & field)
+    {
+        auto equal = [](char x, char y, char z) -> bool { return (x == y) && (y == z); };
+        // rows
+        for (uint8_t j = 0; j < 3; ++j) {
+            if (equal(field[j * 3], field[j * 3 + 1], field[j * 3 + 2])) {
+                return field[j * 3];
+            }
+        }
+        // columns
+        for (uint8_t i = 0; i < 3; ++i) {
+            if (equal(field[i], field[3 + i], field[6 + i])) {
+                return field[i];
+            }
+        }
+        // diagonals
+        if (equal(field[0], field[4], field[8])) {
+            return field[0];
+        }
+        if (equal(field[2], field[4], field[6])) {
+            return field[2];
+        }
+        return ' ';
     }
 
     //-------------------------------------------------------
@@ -243,18 +353,20 @@ public:
     void onBroadcast(const RoomInfo & room, const std::string & message) override
     {
         mHostPlaysCrosses = (message[0] == 'X');
+        //unblockInput();
     }
 
     void onClosed(const RoomInfo & room) override
     {
         std::cout << std::string("Room \"") + room.roomName + "\" is closed!\n";
-        mFinish = true;
+        //mFinish = true;
     }
 
     void onSessionStart(const RoomInfo & room, SessionPtr session) override
     {
         std::cout << std::string("Room \"") + room.roomName + "\" session is started!\n";
         mSession = session;
+        mSession->startup(this, 5 * 1000);
     }
 
 
@@ -269,19 +381,89 @@ public:
     void onLeft(const std::string & playerName, const RoomInfo & room) override
     {
         std::cout << std::string("[") + playerName + "]: I left \"" + room.roomName + "\"\n";
-        mFinish = true;
+        //mFinish = true;
     }
 
     void onBroadcast(const std::string & playerName, const std::string & message) override
     {
-        //std::cout << std::string("[") + playerName + "]: I got broadcast message \"" + message + "\"\n";
         mHostPlaysCrosses = (message[0] == 'X');
+        //unblockInput();
     }
 
     void onSessionStart(const std::string & playerName, SessionPtr session) override
     {
         std::cout << std::string("[") + playerName + "]: I got SessionStarted message!\n";
         mSession = session;
+        mSession->startup(this, 5 * 1000);
+    }
+
+    //-------------------------------------------------------
+    // Session callback
+
+    void onStart(const std::string & sessionName, const std::string & playerName) override
+    {
+        std::cout << std::string("SessionCallback[") + playerName + "]: Started session " + sessionName + "\n";
+        if (mHost != nullptr) {
+            mSession->broadcast("", makeMessage("         ", 0), true);
+        }
+    }
+
+    void onUpdate(const std::string & sessionName, const std::string & playerName, const SessionData & data, const std::string & sharedData) override
+    {
+        std::string field;
+        uint32_t turn;
+        std::tie(field, turn) = parseMessage(sharedData);
+
+        const bool isHost   = (mHost != nullptr);
+        const bool hostTurn = (turn % 2 == 0);
+        const bool myTurn   = (isHost && hostTurn) || (!mHost && !hostTurn);
+        const char mySymbol = ((isHost && mHostPlaysCrosses) || (!mHost && !mHostPlaysCrosses)) ? 'X' : 'O';
+
+        for (;;) {
+            // Check win
+            const char winSym = checkWin(field);
+            int winCode = 0; // 0 - nothing, 1 - win, 2 - loose, 3 - draw
+            if (winSym != ' ') {
+                winCode = (winSym == mySymbol) ? 1 : 2;
+            }
+            if (0 == winCode) {
+                if (field.find(' ') == std::string::npos) {
+                    winCode = 3;
+                }
+            }
+            if (winCode != 0) {
+                mFinish = true;
+            }
+            // Draw screen
+            drawGameScreen(sessionName, field, myTurn, mySymbol, winCode);
+            // Make turn
+            if (!mFinish && myTurn) {
+                std::string s;
+                std::getline(std::cin, s);
+                if (s.size() == 2) {
+                    const char letter = s[0];
+                    const int  number = s[1] - '0';
+                    if (std::string("abc").find(letter) != std::string::npos && number >= 1 && number <= 3) {
+                        const int  idx = (letter - 'a') * 3 + number - 1;
+                        if (field[idx] == ' ') {
+                            field[idx] = mySymbol;
+                            mSession->broadcast(s, makeMessage(field, turn + 1), true);
+                            break;
+                        }
+                    }
+                }
+                std::cout << "Invalid turn" << std::endl;
+                pause(1000);
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    void onSync(const std::string & sessionName, const std::string & playerName, uint32_t syncId) override
+    {
+        std::cout << std::string("SessionCallback[") + playerName + "]: Got sync " + std::to_string(syncId) + "\n";
     }
 };
 
